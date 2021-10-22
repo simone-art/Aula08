ac4c1ad26ee49881662a5c79e46abfcb23c71dcb
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-10-22 20:20:06 -0300

    Desafio 1 solucionado com sucesso

--- src/Desafio01.java
+++ src/Desafio01.java
@@ -12,6 +12,7 @@ public class Desafio01 {
 	 * afirmar se o estado existe ou não, dependendo do que a função retornar.
 	 */
 
+	// equalsIgnoreCase é pra ignorar a palavra digitada em maiúsculas
 	public static boolean validarSiglaEstado(String estado) {
 		String estados[] = { "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB",
 				"PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO" };
